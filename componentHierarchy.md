7a470a5a84f0c58587916243e5d0855b79b20b95
Melissa Elizondo <melissaelizn@gmail.com>
2023-06-05 09:40:52 -0400

    README WIP

---
+++ componentHierarchy.md
@@ -0,0 +1,18 @@
+├── App
+│   ├── Header 
+│   │   ├── ToggleDarkMode // stretch goal
+│   │   ├── LogIn
+│   ├── NavBar
+│   │   ├── Pages
+│   │   │   ├── Register
+│   │   │   ├── MemberList
+│   │   │   ├── ForumList
+│   │   │   ├── Search
+│   │   │   ├── LatestPosts
+│   ├── Content
+│   │   ├── Profile
+│   │   ├── FavoritedForums
+│   │   ├── YourPosts
+│   │   │   ├── Post
+│   └── Footer
+└──
--- server/app.py
+++ server/app.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-
-# Standard library imports
-
 # Remote library imports
-from flask import request
 from flask_restful import Resource
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
 
 # Local imports
 from config import app, db, api
@@ -14,3 +11,5 @@ from models import User, Recipe
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
