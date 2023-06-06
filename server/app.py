3459cc85714639b73162626dc8530c96dbc03c9e
Melissa Elizondo <melissaelizn@gmail.com>
2023-06-05 21:14:39 -0400

    database

--- server/app.py
+++ server/app.py
@@ -5,11 +5,34 @@ from flask_migrate import Migrate
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import User
 
 # Views go here!
 
+@app.route('/')
+def index():
+    return '<h1> Testing </h1>'
+
+
+@app.route('/register', methods=[ 'POST' ])
+def register():
+    if request.method == 'POST':
+        request = request.get_json()
+
+        new_user = User(
+            
+        )
+
+
+        avatar = request.files['file']
+        upload = upload(filename = avatar.filename, data = avatar.read())
+
+        db.session.add(upload)
+        db.session.commit()
+
+        return f'Uploaded: {avatar.filename}'
+    return
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
 
--- server/models.py
+++ server/models.py
@@ -6,8 +6,84 @@ from sqlalchemy_serializer import SerializerMixin
 from flask import abort 
 from config import db, metadata
 
+import re, imghdr
 
 class User( db.Model, SerializerMixin):
-    pass
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    _password = db.Column(db.String)
+    avatar = db.Column(db.LargeBinary)
+
+    created_at = db.Column( db.DateTime, server_default=db.func.now() )
+    updated_at = db.Column( db.DateTime, onupdate=db.func.now() )
+
+    posts = db.relationship( 'Forum', backref = 'user' )
+    # forum = association_proxy( 'posts', 'forum' )
+
+
+    @validates('username')
+    def validate_username( self, attr, username ):
+        if type(username) is str and len(username) in range(5, 16) and re.match(r'^[A-Za-z0-9_]+$', username):
+            return username 
+        else:
+            abort( "Username must be a string between 5 - 15 characters and not contain any special characters. ")
+
+    @validates('email')
+    def validate_email( self, attr, email ):
+        if "@" and ".com" not in email:
+            abort( "Must be a valid email address.")
+        return email
+    
+    @validates('avatar')
+    def validate_avatar( self, attr, avatar ):
+        if avatar is not None:
+            file_format = imghdr.what(None, h= avatar)
+            if file_format != 'jpeg':
+                abort ("Only JPEG images are permitted.")
+        return avatar.filename
+
+    @validates( 'password' )
+    def validate_password( self, attr, password ):
+        if type(password) is str and len(password) in range(4, 16):
+            return "Password has been set."
+        else:
+            abort( "Password must be a string between 4 - 15 characters. ")
+
+    def __repr__(self):
+        return f'<User {self.id}: {self.username}>'
+    
+class Post( db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    image = db.Column(db.LargeBinary)
+
+    created_at = db.Column( db.DateTime, server_default=db.func.now() )
+    updated_at = db.Column( db.DateTime, onupdate=db.func.now() )
+
+    user_id = db.Column( db.Integer, db.ForeignKey( 'user.id' ) )
+    forum_id = db.Column( db.Integer, db.ForeignKey( 'forum.id' ) )
+
+    @validates('image')
+    def validate_avatar( self, attr, image ):
+        if image is not None:
+            file_format = imghdr.what(None, h= image)
+            if file_format != 'jpeg':
+                abort ("Only JPEG images are permitted.")
+        return image.filename
+
+    @validates( 'title' )
+    def validate_title( self, attr, title ):
+        if type(title) is str and len(title) in range(4, 16):
+            return title
+        else:
+            abort( "Password must be a string between 4 - 15 characters. ")
+
+    def __repr__(self):
+        return f'<Post {self.id}: {self.title}>'
+
 
 # Models go here!
