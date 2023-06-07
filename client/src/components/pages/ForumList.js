efacfd9da432e35c6cd364ffd9fc10aa763b2407
Melissa Elizondo <melissaelizn@gmail.com>
2023-06-07 04:07:40 -0400

    components

---
+++ client/src/components/pages/ForumList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+function ForumList(){
+  const [ forum, setForum ] = useState([])
+  
+  useEffect( () => {
+    fetch("http://127.0.0.1:5555/forums")
+    .then((r) => r.json())
+    .then( data => setForum(data) ) }, []
+  )
+
+    const renderForumCards = forum.map( forum => 
+      <div>
+          <h2> {forum.title} </h2>
+          <h3> {forum.content} </h3>
+      </div>
+    )
+
+    return (
+      <main > 
+        <div className="forum-list">
+          <h1 className="all-forum"> </h1>
+            {renderForumCards}
+        </div>
+      </main>
+    )
+
+}
+
+export default ForumList;
---
+++ client/src/components/pages/MemberList.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+import React, { useState, useEffect } from "react";
+
+function MemberList(){
+  const [ members, setMembers ] = useState([])
+  
+  useEffect( () => {
+    fetch("http://127.0.0.1:5555/users")
+    .then((r) => r.json())
+    .then( data => setMembers(data) ) }, []
+  )
+  
+    const renderMemberCards = members.map( member => 
+      <div>
+          <h2> User: {member.username} </h2>
+      </div>
+    )
+
+    return (
+      <main > 
+        <div className="member-list">
+          <h1 className="all-members"> </h1>
+            {renderMemberCards}
+        </div>
+      </main>
+    )
+
+}
+
+export default MemberList;
